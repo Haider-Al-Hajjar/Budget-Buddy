a008f16a32e157a84ba1455f3e4df187688847ce
Haider Al Hajjar <alhajjarhaider@gmail.com>
2023-12-15 17:41:09 -0500

    Working on remove button, succeeded on getting the submit button to work.

--- script.js
+++ script.js
@@ -2,28 +2,58 @@ let expenseName = document.getElementById("expense__name")
 let expenseAmount = document.getElementById("expense__amount")
 let expenseBtn = document.getElementById("expense__submit")
 let expenseContainer = document.getElementById("expense")
-let expenseOutput = document.getElementById("expense__Ouput")
+let expenseOutput = document.getElementById("expense__output")
 let classArray = []
+let idArray = []
 let className = expenseName.value.replace(/ /g, "")
 // Previos lines create variables to refer to the divs, inputs, output, and an array to hold class names. This way, they can be sorted.
 expenseBtn.addEventListener("click", function addExpense() {
+    const newExpense = document.createElement("div")
     const newExpenseName = document.createElement("div")
-    newExpenseName.classList.add("test", expenseName.value.replace(/ /g, ""))
-    if (classArray.indexOf(expenseName.value) == -1) {
-        classArray.push(expenseName.value)
+    const newClassName = expenseName.value.replace(/ /g, "")
+
+    newExpense.classList.add("expense__newExpense")
+    newExpenseName.classList.add("expense__item", newClassName)
+    if (classArray.indexOf(newClassName) == -1) {
+        classArray.push(newClassName)
     }
     newExpenseName.style.order = classArray.length
     // This checks whether or not the class name is unique, (thus a new class entirely), and if so, adds it to the list of classes.
     // This is for sorting later.
+
+    // classArray =
+    //     newExpenseName.setAttribute("id",)
+
+
     const newExpenseNameText = document.createTextNode(expenseName.value)
     newExpenseName.appendChild(newExpenseNameText)
-    expenseOutput.appendChild(newExpenseName)
+    newExpense.appendChild(newExpenseName)
+
     // This creates a div with the text that the person put into the text field and a class of what was put into the text field.
+
     const newExpenseAmount = document.createElement("div")
-    newExpenseAmount.classList.add("test2")
+    newExpenseAmount.classList.add("expense__cost")
     const newExpenseAmountNumber = document.createTextNode(expenseAmount.value)
     newExpenseAmount.appendChild(newExpenseAmountNumber)
-    expenseOutput.appendChild(newExpenseAmount)
+    newExpenseAmount.style.order = classArray.length
+
+    newExpense.appendChild(newExpenseAmount)
     // This creates a div with the text the person put into the number field.
 
+    let removeExpenseBtn = document.createElement("button")
+    removeExpenseBtn.classList.add("remove__expense",)
+    const removeExpenseTxt = document.createTextNode("Remove")
+    removeExpenseBtn.appendChild(removeExpenseTxt)
+    removeExpenseBtn.style.order = classArray.length
+    newExpense.appendChild(removeExpenseBtn)
+    // This creates a button with the teext "Remove" & a class of "remove__expense" to be used in functions later.
+
+    expenseOutput.appendChild(newExpense)
 })
+expenseBtn.addEventListener("click", function removeExpense() {
+    console.log(true)
+})
+// let removeExpenseBtn = document.getElementsByClassName("remove__expense")
+// removeExpenseBtn.addEventListener("click", function removeExpense() {
+//     console.log(true)
+// })
